f651c4ff283690e814ca298309e2f31ac212e8d4
Avinash <avinasirs@wekancode.com>
2020-07-30 16:13:07 +0530

    inv panel evaluation form validations added

--- src/app/custom-form-validators/validation.ts
+++ src/app/custom-form-validators/validation.ts
@@ -1,7 +1,19 @@
 'use strict';
 import { Validators } from '@angular/forms';
+const alphaNumericMaxLength: RegExp = /^([a-zA-Z0-9_ ]){0,255}$/;
+const alphaNumericMaxLength30: RegExp = /^([a-zA-Z0-9_ ]){0,30}$/;
+const alphaWithDot: RegExp = /^[a-zA-Z\.]*$/;
 
 export const textVal: any = [
-    Validators.pattern(/^(.|\s)*\S(.|\s)*$/),
-    Validators.required
+  Validators.pattern(/^(.|\s)*\S(.|\s)*$/),
+  Validators.required,
+  Validators.pattern(alphaNumericMaxLength),
 ];
+
+export const req: any = Validators.required;
+
+export const alphaNum255: any = Validators.pattern(alphaNumericMaxLength);
+
+export const alphaNum30: any = Validators.pattern(alphaNumericMaxLength30);
+
+export const alphaWithDots: any = Validators.pattern(alphaWithDot);
--- src/app/interview-panel-dashboard/evaluation-form/evaluation-form.component.ts
+++ src/app/interview-panel-dashboard/evaluation-form/evaluation-form.component.ts
@@ -5,6 +5,7 @@ import { FormControl, FormGroup, FormBuilder, NgModel, Validators, FormArray } f
 import * as myGlobals from '../../custom-form-validators/validation';
 import { AdminServiceService } from 'src/app/services/admin-service.service';
 import { AppConfigService } from 'src/app/config/app-config.service';
+import { RemoveWhitespace } from 'src/app/custom-form-validators/removewhitespace.js';
 export interface PeriodicElement {
   id: number;
   name: string;
@@ -31,7 +32,7 @@ export class EvaluationFormComponent implements OnInit {
     private formBuilder: FormBuilder,
     private adminService: AdminServiceService,
     private appConfig: AppConfigService,
-    ) {
+  ) {
     this.getEvaluationData();
   }
   get f() {
@@ -39,35 +40,49 @@ export class EvaluationFormComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.nginitFunc();
+  }
+
+  nginitFunc() {
     console.log(SampleJson);
     this.dataSource = new MatTableDataSource<PeriodicElement>(this.assessments);
     this.evaluationForm = this.formBuilder.group({
       interview_date: new FormControl(''),
-      interview_place: new FormControl(''),
-      willing_work: new FormControl(''),
-      physical_disability: new FormControl(''),
-      candidates_strenght: ['', myGlobals.textVal],
-      candidates_weakness: new FormControl(''),
-      panel_member1: new FormControl(''),
-      panel_member2: new FormControl(''),
-      panel_member3: new FormControl(''),
-      panel_member4: new FormControl(''),
-      ps_no1: new FormControl(''),
-      ps_no2: new FormControl(''),
-      ps_no3: new FormControl(''),
-      ps_no4: new FormControl(''),
-      topic_given: new FormControl(''),
-      thought: new FormControl(''),
-      content: new FormControl(''),
-      language: new FormControl(''),
-      idea: new FormControl(''),
-      clues: new FormControl(''),
-      time_taken: new FormControl(''),
-      remarks: new FormControl(''),
-      ASSESSMENT: new FormControl(''),
+      interview_place: new FormControl('', [RemoveWhitespace.whitespace(), myGlobals.req, myGlobals.alphaNum30]),
+      willing_work: new FormControl('', [Validators.required]),
+      physical_disability: new FormControl('', [Validators.maxLength(50)]),
+      candidates_strenght: ['', [Validators.maxLength(100)]],
+      candidates_weakness: new FormControl('', [Validators.maxLength(100)]),
+      panel_member1: new FormControl('', [myGlobals.alphaWithDots, Validators.maxLength(50)]),
+      panel_member2: new FormControl('', [myGlobals.alphaWithDots, Validators.maxLength(50)]),
+      panel_member3: new FormControl('', [myGlobals.alphaWithDots, Validators.maxLength(50)]),
+      panel_member4: new FormControl('', [myGlobals.alphaWithDots, Validators.maxLength(50)]),
+      ps_no1: new FormControl('', [Validators.maxLength(30)]),
+      ps_no2: new FormControl('', [Validators.maxLength(30)]),
+      ps_no3: new FormControl('', [Validators.maxLength(30)]),
+      ps_no4: new FormControl('', [Validators.maxLength(30)]),
+      topic_given: new FormControl('', [RemoveWhitespace.whitespace(), Validators.required, Validators.maxLength(100)]),
+      thought: new FormControl('', [Validators.maxLength(100)]),
+      content: new FormControl('', [Validators.maxLength(100)]),
+      language: new FormControl('', [Validators.maxLength(100)]),
+      idea: new FormControl('', [Validators.maxLength(100)]),
+      clues: new FormControl('', [Validators.maxLength(100)]),
+      time_taken: new FormControl('', [Validators.maxLength(30)]),
+      remarks: new FormControl('', [RemoveWhitespace.whitespace(), Validators.required, Validators.maxLength(100)]),
+      ASSESSMENT: new FormControl('', [Validators.required]),
       depth_knowledge: new FormControl(''),
       uid: '1091'
     });
+  }
+
+  get interview_place() {
+    return this.evaluationForm.get('interview_place');
+  }
+  get candidates_strenght() {
+    return this.evaluationForm.get('candidates_strenght');
+  }
+  get candidates_weakness() {
+    return this.evaluationForm.get('candidates_weakness');
   }
 
   setAssessmentLevel(assessment, value) {
@@ -97,10 +112,26 @@ export class EvaluationFormComponent implements OnInit {
   }
 
   submitEvaluationForm() {
-    console.log(this.evaluationForm.value);
-    this.adminService.postEvaluationCandidateData(this.evaluationForm.value).subscribe((res: any) => {
-      this.appConfig.hideLoader();
-      console.log(res);
+    if (this.evaluationForm.valid) {
+      console.log(this.evaluationForm.value);
+      this.adminService.postEvaluationCandidateData(this.evaluationForm.value).subscribe((res: any) => {
+        this.appConfig.hideLoader();
+        console.log(res);
+      });
+    } else {
+      this.validateAllFields(this.evaluationForm);
+    }
+  }
+
+  // To validate all fields after submit
+  validateAllFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFields(control);
+      }
     });
   }
 
