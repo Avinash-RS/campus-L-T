4e006b8a4d49954e60f7fb28001c0f45aa17e356
Avinash <avinasirs@wekancode.com>
2020-08-18 16:51:47 +0530

    asteric symbol added in candidate documents page

--- utl.ts
+++ utl.ts
@@ -1,27 +1,48 @@
-http://udapqa.southindia.cloudapp.azure.com/d8cintana2/profile/getassement_candidate_shortlist
+const defect = `
+8 - resolved
+13 - resolved
+15 - resolved
+30 - resolved
+33 - not a bug
+82 - resolved
+83 - resolved
+84 - resolved
+85 - resolved
+89 - resolved
+107 - resolved (we have removed reject option itself)
+136 - resolved
+152 - not a bug (when you go into particular candidate assessment details, tag name there represents the tag name which that candidate mapped into)
+200 - resolved
+201 - resolved
+207 - resolved
+`
 
+const action = `
+52 - resolved
+53 - resolved
+54 - resolved
+57 - resolved
+61 - resolved
+64 - resolved
+76 - resolved
+77 - resolved (reject option removed)
+79 - resolved
+80 - resolved (let me know if this issue persists in any other screens)
+88 - resolved
+`
 
-.mat-checkbox-checked.mat-accent .mat-checkbox-background, .mat-checkbox-indeterminate.mat-accent .mat-checkbox-background {
-  background-color: #ff4081;
-}
-{
-  003f72
-  "types:"education or resume or other or certificate",
-  "id": document_id
-}
+Admin Credentials:
+username: avinasi.s@cintanatech.com
+password: Cintana@123
 
-  Admin Credentials:
-  username: avinasi.s@cintanatech.com
-  password: Cintana@123
+HR Credentials:
+username: avinashcareers29@gmail.com
+password: Cintana@123
 
-  HR Credentials:
-  username: avinashcareers29@gmail.com
-  password: Cintana@123
+Candidate Credentials:
+username: avinasiselvakumar01@gmail.com
+password: Cintana@123
 
-  Candidate Credentials:
-  username: avinasiselvakumar01@gmail.com
-  password: Cintana@123
-
-  TPO Credentials:
-  username: zsrm@mailinator.com
-  password: admin
+TPO Credentials:
+username: zsrm@mailinator.com
+password: admin
