9a3fc88b16b32c18a85dfb4bc5e53c8f0be23efd
Avinash <avinasirs@wekancode.com>
2020-08-18 16:12:01 +0530

    custom template model added in hr over all reports

--- src/app/admin-dashboard/admin-report/admin-report-list/admin-report-list.component.ts
+++ src/app/admin-dashboard/admin-report/admin-report-list/admin-report-list.component.ts
@@ -48,7 +48,7 @@ export const MY_FORMATS = {
 })
 export class AdminReportListComponent implements OnInit {
 
-  displayedColumns: any[] = ['reportname', 'col2', 'col3','col4', 'fdate', 'tdate', 'action'];
+  displayedColumns: any[] = ['reportname', 'col2', 'col3', 'col4', 'fdate', 'tdate', 'action'];
   dataSource: MatTableDataSource<any>;
 
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
@@ -58,78 +58,82 @@ export class AdminReportListComponent implements OnInit {
   userList;
   tagNameDropdown: any = [];
   institutesList = DropdownListForKYC['institutes'];
-  
+  dro;
   constructor(private appConfig: AppConfigService,
     private apiService: ApiServiceService,
     private adminService: AdminServiceService,
     private sharedService: SharedServiceService) { }
 
   ngOnInit() {
-  //   this.form = new FormGroup({
-  //     title: new FormControl()
-  //  });
+    //   this.form = new FormGroup({
+    //     title: new FormControl()
+    //  });
 
     this.getUsersList();
     this.getTagName();
   }
 
+  detectSelectChange() {
+    this.dro = 'adadad';
+  }
+
   // To get all users
   getUsersList() {
     // this.adminService.userList().subscribe((data: any) => {
-      // this.appConfig.hideLoader();
-      const data = [{
-        'reportname': 'abc',
-        'col2': 'a',
-        'col3': 'b',
-        'col4': 'c',
-        'fdate': 'd',
-        'tdate': 'e',
-        'action': 'f'
-      },
-      {
-        'reportname': 'x',
-        'col2': 'a',
-        'col3': 'b',
-        'col4': 'c',
-        'fdate': 'd',
-        'tdate': 'e',
-        'action': 'f'
-      },
-      {
-        'reportname': 'y',
-        'col2': 'a',
-        'col3': 'b',
-        'col4': 'c',
-        'fdate': 'd',
-        'tdate': 'e',
-        'action': 'f'
-      },
-      {
-        'reportname': 'y',
-        'col2': 'a',
-        'col3': 'b',
-        'col4': 'c',
-        'fdate': 'd',
-        'tdate': 'e',
-        'action': 'f'
-      },
-      {
-        'reportname': 'y',
-        'col2': 'a',
-        'col3': 'b',
-        'col4': 'c',
-        'fdate': 'd',
-        'tdate': 'e',
-        'action': 'f'
-      }
+    // this.appConfig.hideLoader();
+    const data = [{
+      'reportname': 'abc',
+      'col2': 'a',
+      'col3': 'b',
+      'col4': 'c',
+      'fdate': 'd',
+      'tdate': 'e',
+      'action': 'f'
+    },
+    {
+      'reportname': 'x',
+      'col2': 'a',
+      'col3': 'b',
+      'col4': 'c',
+      'fdate': 'd',
+      'tdate': 'e',
+      'action': 'f'
+    },
+    {
+      'reportname': 'y',
+      'col2': 'a',
+      'col3': 'b',
+      'col4': 'c',
+      'fdate': 'd',
+      'tdate': 'e',
+      'action': 'f'
+    },
+    {
+      'reportname': 'y',
+      'col2': 'a',
+      'col3': 'b',
+      'col4': 'c',
+      'fdate': 'd',
+      'tdate': 'e',
+      'action': 'f'
+    },
+    {
+      'reportname': 'y',
+      'col2': 'a',
+      'col3': 'b',
+      'col4': 'c',
+      'fdate': 'd',
+      'tdate': 'e',
+      'action': 'f'
+    }
     ];
-      this.userList = data;
-      // this.userList.forEach(element => {
-      //   element.checked = false;
-      // });
-      this.dataSource = new MatTableDataSource(this.userList);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+    this.userList = data;
+    // this.userList.forEach(element => {
+    //   element.checked = false;
+    // });
+    this.dataSource = new MatTableDataSource(this.userList);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
     // }, (err) => {
     // });
   }
@@ -145,9 +149,9 @@ export class AdminReportListComponent implements OnInit {
   getTagName() {
     this.adminService.getTagName().subscribe((data: any) => {
       this.appConfig.hideLoader();
-      
+
       this.tagNameDropdown = data;
-    
+
     }, (err) => {
     });
   }
