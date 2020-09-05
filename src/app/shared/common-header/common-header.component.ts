1508fede7481db9a8e5c110c2b49e15d2b794dcf
Avinash keshari <avinashke15@gmail.com>
2020-09-05 11:02:12 +0530

    bug fixed

--- src/app/shared/common-header/common-header.component.ts
+++ src/app/shared/common-header/common-header.component.ts
@@ -32,7 +32,7 @@ export class CommonHeaderComponent implements OnInit {
     const data = {
       iconName: '',
       sharedData: {
-        confirmText: 'Are you sure you want to Logout?',
+        confirmText: 'Are you sure you want to logout?',
         componentData: '',
         type: 'delete',
         identity: 'logout'
