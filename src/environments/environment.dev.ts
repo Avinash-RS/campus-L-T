1960fa7d1b181d882a44ad0549797bbfafa37628
avinasi <avinasi@lntecc.com>
2020-09-25 15:57:01 +0530

    dev endpoint changed
    dev endpoint changed

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,8 +4,11 @@ export const environment = {
   dev: true,
 
   // For Dev
-  API_BASE_URL: 'http://104.211.226.77/d8cintana2',
-  API_BASE_URL_city: 'http://104.211.226.77/d8cintana2',
+  // API_BASE_URL: 'http://104.211.226.77/d8cintana2',
+  // API_BASE_URL_city: 'http://104.211.226.77/d8cintana2',
+  // Image_Base_Url: 'http://104.211.226.77',
+  API_BASE_URL: 'http://104.211.226.77/DRUPA_DEV_HEADLESS',
+  API_BASE_URL_city: 'http://104.211.226.77/DRUPA_DEV_HEADLESS',
   Image_Base_Url: 'http://104.211.226.77',
 
   // For QA
