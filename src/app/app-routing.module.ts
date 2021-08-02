f7fdc9655601a652ae7cccf689fc9bdefd25168f
Avinasi <AVINASI@lntecc.com>
2021-08-02 16:22:16 +0530

    Scroll issue on navigation has been fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true, scrollOffset: [0, 0], scrollPositionRestoration: 'top' })],
+  imports: [RouterModule.forRoot(routes, {useHash: true, scrollPositionRestoration: 'top' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
