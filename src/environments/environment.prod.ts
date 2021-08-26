5dd04a4c06098c70999f3e2c1cd91bed03bddc92
Chokkalingam K <CHOKKALINGAMK@lntecc.com>
2021-08-26 13:41:34 +0000

    Updated campus dev environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,39 +1,16 @@
 export const environment = {
-  production: true,
-  qa: false,
+  production: false,
   uat: false,
+  qa: false,
+  dev: true,
   local: false,
   ASSETS: 'https://assets.lntedutech.com',
 
-  versionCheckURL: 'https://campus.lntedutech.com/version.json',
-
+  versionCheckURL: 'https://campus-dev.lntedutech.com/version.json',
   // For Dev
-  // API_BASE_URL: 'http://104.211.226.77/d8cintana2',
-  // API_BASE_URL_city: 'http://104.211.226.77/d8cintana',
-  // Image_Base_Url: 'http://104.211.226.77',
-
-  // For QA
-  // API_BASE_URL: 'http://udapqa.southindia.cloudapp.azure.com/d8cintana2',
-  // API_BASE_URL_city: 'http://udapqa.southindia.cloudapp.azure.com/d8cintana2',
-  // Image_Base_Url: 'http://udapqa.southindia.cloudapp.azure.com',
-
-  // For Prod
-  // API_BASE_URL: 'http://udapprod.southindia.cloudapp.azure.com/d8cintana2',
-  // API_BASE_URL_city: 'http://udapprod.southindia.cloudapp.azure.com/d8cintana2',
-  // Image_Base_Url: 'http://udapprod.southindia.cloudapp.azure.com',
-  // API_BASE_URL: 'https://campus.lntedutech.com/d8cintana2',
-  // API_BASE_URL_city: 'https://campus.lntedutech.com/d8cintana2',
-  // Image_Base_Url: 'https://campus.lntedutech.com',
-  // API_BASE_URL: 'http://40.119.225.78/d8cintana2',
-  // API_BASE_URL_city: 'http://40.119.225.78/d8cintana2',
-  // Image_Base_Url: 'http://40.119.225.78',
-  // API_BASE_URL: 'https://campus-prod.lntedutech.com/d8cintana2',
-  // API_BASE_URL_city: 'https://campus-prod.lntedutech.com/d8cintana2',
-  // Image_Base_Url: 'https://campus-prod.lntedutech.com',
-  NODE_API_BASE_URL: 'https://candidateuat.lntedutech.com',
-  WEBRTC_NODE_API : 'https://uapuatapplicationgateway.lntedutech.com',
-  API_BASE_URL: 'https://campus.lntedutech.com/d8cintana2',
-  API_BASE_URL_city: 'https://campus.lntedutech.com/d8cintana2',
-  Image_Base_Url: 'https://campus.lntedutech.com',
-
+  NODE_API_BASE_URL: 'https://candidate.lntiggnite.com',
+  WEBRTC_NODE_API : 'https://uapapplicationgateway.lntiggnite.com',
+  API_BASE_URL: 'https://campus-dev.lntedutech.com/dcampus',
+  API_BASE_URL_city: 'https://campus-dev.lntedutech.com/dcampus',
+  Image_Base_Url: 'https://campus-dev.lntedutech.com'
 };
