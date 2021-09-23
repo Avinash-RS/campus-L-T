8b30709a4e06d78c89c61d90ca389a928fa5bc01
Avinasi <AVINASI@lntecc.com>
2021-09-23 18:47:04 +0530

    skip kyc for lateral entries

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -4,6 +4,7 @@ export const environment = {
     uat: true,
     local: false,
     ASSETS: 'https://assets.lntedutech.com',
+    //  Existing UAT
     versionCheckURL: 'https://uat-campus.lntedutech.com/version.json',
 
     NODE_API_BASE_URL: 'https://candidateuat.lntedutech.com',
@@ -11,6 +12,15 @@ export const environment = {
     API_BASE_URL: 'https://uat-campus.lntedutech.com/d8cintana2',
     API_BASE_URL_city: 'https://uat-campus.lntedutech.com/d8cintana2',
     Image_Base_Url: 'https://uat-campus.lntedutech.com',
+
+    // New C2e Instance
+    // versionCheckURL: 'https://c2e.lntedutech.com/version.json',
+
+    // NODE_API_BASE_URL: 'https://candidate.lntedutech.com',
+    // WEBRTC_NODE_API : 'https://uapapplicationgateway.lntedutech.com',
+    // API_BASE_URL: 'https://c2e.lntedutech.com/d8cintana2',
+    // API_BASE_URL_city: 'https://c2e.lntedutech.com/d8cintana2',
+    // Image_Base_Url: 'https://c2e.lntedutech.com',
 
 
   };
