eb07c4cc0454de95b0fe3f251184085a5a45be98
Avinasi <AVINASI@lntecc.com>
2021-10-07 14:56:54 +0530

    No interview scheduled pop up added

--- src/app/config/interceptors.service.ts
+++ src/app/config/interceptors.service.ts
@@ -139,8 +139,10 @@ export class InterceptorsService implements HttpInterceptor {
         //   return throwError(error);
         // }
         if (error.status === 404) {
-          this.appConfig.error(error.error.FailureReason ? error.error.FailureReason.message : error.error.message
-            ? error.error.message : '404 Not found', '');
+          if (error && error.error && error.error.FailureReason && !error.error.FailureReason.message.includes('yet to submit his/her profile')) {
+            this.appConfig.error(error.error.FailureReason ? error.error.FailureReason.message : error.error.message
+              ? error.error.message : '404 Not found', '');
+          }
           return throwError(error);
         }
         if (error.status === 409) {
