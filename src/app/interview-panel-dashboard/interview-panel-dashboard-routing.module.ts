dd0b9209cc8117648f9a1d67e4de26aafd379ae5
Avinasi <AVINASI@lntecc.com>
2021-11-22 15:40:38 +0530

    unwanted component declaration deletion in interviewpanel home routing

--- src/app/interview-panel-dashboard/interview-panel-dashboard-routing.module.ts
+++ src/app/interview-panel-dashboard/interview-panel-dashboard-routing.module.ts
@@ -5,14 +5,8 @@ import { CONSTANT } from '../constants/app-constants.service';
 import { InvCandidateDetailsComponent } from './inv-candidate-details/inv-candidate-details.component';
 import { InvParticularAssessmentCandidatesComponent } from './inv-candidate-details/inv-particular-assessment-candidates/inv-particular-assessment-candidates.component';
 import { InvEvaluationMainScreenComponent } from './inv-candidate-details/inv-evaluation-main-screen/inv-evaluation-main-screen.component';
-import { InvSubAssessmentsComponent } from './inv-candidate-details/inv-evaluation-main-screen/inv-sub-assessments/inv-sub-assessments.component';
-import { InvSubEducationComponent } from './inv-candidate-details/inv-evaluation-main-screen/inv-sub-education/inv-sub-education.component';
-import { InvSubEmploymentComponent } from './inv-candidate-details/inv-evaluation-main-screen/inv-sub-employment/inv-sub-employment.component';
-import { InvSubEvaluateComponent } from './inv-candidate-details/inv-evaluation-main-screen/inv-sub-evaluate/inv-sub-evaluate.component';
 import { EvaluationFormComponent } from './evaluation-form/evaluation-form.component';
 import { InvpanelGuard } from '../guards/canload/invpanel.guard';
-import { InvUnifiedreportsComponent } from './inv-candidate-details/inv-evaluation-main-screen/inv-unifiedreports/inv-unifiedreports.component';
-import { InvJoinInterviewComponent } from './inv-candidate-details/inv-evaluation-main-screen/inv-join-interview/inv-join-interview.component';
 
 
 const routes: Routes = [
@@ -41,56 +35,7 @@ const routes: Routes = [
             component: InvEvaluationMainScreenComponent,
             data: {
               breadcrumb: 'Evaluation'
-            },
-            // children: [
-            //   {
-            //     path: `${CONSTANT.ROUTES.INTERVIEW_PANEL_DASHBOARD.SUB_ASSESSMENTS}`,
-            //     component: InvSubAssessmentsComponent,
-            //     data: {
-            //       breadcrumb: 'Assessment Details'
-            //     }
-            //   },
-            //   {
-            //     path: `${CONSTANT.ROUTES.INTERVIEW_PANEL_DASHBOARD.SUB_EDUCATION}`,
-            //     component: InvSubEducationComponent,
-            //     data: {
-            //       breadcrumb: 'Document Details'
-            //     }
-            //   },
-            //   {
-            //     path: `${CONSTANT.ROUTES.INTERVIEW_PANEL_DASHBOARD.SUB_EMPLOYMENT}`,
-            //     component: InvSubEmploymentComponent,
-            //     data: {
-            //       breadcrumb: 'Candidate Profile'
-            //     }
-            //   },
-            //   {
-            //     path: `${CONSTANT.ROUTES.INTERVIEW_PANEL_DASHBOARD.SUB_EVALUATION}`,
-            //     component: InvSubEvaluateComponent,
-            //     data: {
-            //       breadcrumb: 'Evaluation Details'
-            //     }
-            //   },
-            //   {
-            //     path: `${CONSTANT.ROUTES.INTERVIEW_PANEL_DASHBOARD.UNIFIEDREPORTS}`,
-            //     component: InvUnifiedreportsComponent,
-            //     data: {
-            //       breadcrumb: 'Test Details'
-            //     }
-            //   },
-            //   {
-            //     path: `${CONSTANT.ROUTES.INTERVIEW_PANEL_DASHBOARD.JOIN_INTERVIEW}`,
-            //     component: InvJoinInterviewComponent,
-            //     data: {
-            //       breadcrumb: 'Join Sessions'
-            //     }
-            //   },
-            //   {
-            //     path: '',
-            //     redirectTo: `${CONSTANT.ROUTES.INTERVIEW_PANEL_DASHBOARD.SUB_EMPLOYMENT}`,
-            //     pathMatch: 'full',
-            //   }
-            // ]
+            }
           },
           {
             path: '',
@@ -111,8 +56,6 @@ const routes: Routes = [
         data: {
           breadcrumb: 'Evaluation Form'
         }
-        // redirectTo: `${CONSTANT.ROUTES.INTERVIEW_PANEL_DASHBOARD.EVALUATION_FORM}`,
-        // pathMatch: 'full',
       }
     ]
   }
