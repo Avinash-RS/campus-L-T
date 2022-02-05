1c8a4f1842ef20293fc4f90962509a00318f1e59
Gokul Appaswamy <77961879+GokulALT@users.noreply.github.com>
2022-02-05 16:13:10 +0530

    interview status column added

--- src/app/hr-dashboard/evaluation/new-interviewpanel-assigned-details/assigned-details/assigned-details.component.ts
+++ src/app/hr-dashboard/evaluation/new-interviewpanel-assigned-details/assigned-details/assigned-details.component.ts
@@ -58,6 +58,8 @@ export class AssignedDetailsComponent implements OnInit, AfterViewInit, OnDestro
   filterValue: string;
   reportDetails1: any;
   rowData1: any;
+  customerCode = this.appConfig.getSelectedCustomerCode();
+
   // quickSearchValue: string;
   quickSearchValue = '';
   quickSearchValue1 = '';
@@ -190,7 +192,7 @@ export class AssignedDetailsComponent implements OnInit, AfterViewInit, OnDestro
   }
 
   withoutVideoSchedule() {
-    return [
+    let colVal:any = [
       {
         headerName: 'Candidate Id', field: 'candidate_id',
         filter: 'agNumberColumnFilter',
@@ -327,6 +329,38 @@ export class AssignedDetailsComponent implements OnInit, AfterViewInit, OnDestro
       //   }
       // },
     ];
+    if( this.customerCode == '#ADANI'){
+      let adanistatus:any =  {
+        headerName: 'Status', field: 'interview_status',
+        filter: 'agTextColumnFilter',
+        minWidth: 140,
+        sortable: false,
+        tooltipField: 'interview_status',
+        getQuickFilterText: (params) => {
+          return params.value;
+        }
+        ,
+        cellRenderer: (params:any) => {
+          console.log(params)
+          if (
+            params.data && (params.data.interview_status == "Can be considered")
+          ) {
+              return `<button class="btn-outline checked inprogress-bg"><em class="icon-checked"></em>${params.data.interview_status}</button>`;
+          }
+          if (params.data && (params.data.interview_status) =="Selected") {
+            return `<button class="btn-outline checked completed-bg"><em class="icon-checked"></em>${params.data.interview_status}</button>`;
+          }
+          if(params.data.interview_status == "" || params.data.interview_status == null) {
+            return `<button class="btn-outline">Yet to evaluate</button>`;
+          }
+          if(params.data.interview_status == "Not Selected") {
+            return `<button class="rejected btn-outline checked " click=""><em class="icon-close_black"></em>${params.data.interview_status}</button>`;
+          }
+        }
+      }
+      colVal.splice(6, 0, adanistatus);
+    }
+    return colVal
   }
 
   WithVideoSchedulingColumns() {
@@ -355,7 +389,10 @@ export class AssignedDetailsComponent implements OnInit, AfterViewInit, OnDestro
     }
 
   onCellClicked(event) {
-    if (event.colDef.field === 'name') {
+    if (event.colDef.field === 'name'||event.colDef.field === 'interview_status') {
+      if(event.colDef.field === 'interview_status'){
+        this.appConfig.setLocalData('tabIndex',2)
+      }
       const param = {
         shortlist_name: event['data'] && event['data']['shortlist_name'] ? event['data']['shortlist_name'] : '',
         cid: event['data'] && event['data']['candidate_id'] ? event['data']['candidate_id'] : '',
