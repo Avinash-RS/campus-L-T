6896031098817192a5dc17948c0ef42c35541abc
Avinasi <AVINASI@lntecc.com>
2022-03-31 16:34:34 +0530

    #20179, #20180, #20181: Mandatory for CA level institute, Offer date and reference non-mandatory for General customer and work details date picker max date range increased to 2 years

--- src/app/candidate-dashboard/larsen-module/candidate-joining-form/joining-work-details/joining-work-details.component.ts
+++ src/app/candidate-dashboard/larsen-module/candidate-joining-form/joining-work-details/joining-work-details.component.ts
@@ -249,8 +249,7 @@ export class JoiningWorkDetailsComponent implements OnInit, AfterViewInit, OnDes
     // Set the minimum to January 1st 20 years in the past and December 31st a year in the future.
     const currentYear = new Date().getFullYear();
     this.minDate = new Date(currentYear - 50, 0, 1);
-    this.maxDate = new Date();
-    this.maxDate.setDate(this.maxDate.getDate() + 1);
+    this.maxDate = new Date(currentYear + 2, 11, 31);
   }
 
   momentForm(date) {
