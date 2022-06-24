83eb29cd68ae87dc79621d78b41ae15e4be38434
Avinash <Avinasi.selvakumar@lntedutech.com>
2022-06-24 11:54:29 +0530

    Miscellaneous Tab Structure Created

---
+++ src/app/hr-dashboard/pages/misc-candidate-name-change/misc-candidate-name-change.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-misc-candidate-name-change',
+  templateUrl: './misc-candidate-name-change.component.html',
+  styleUrls: ['./misc-candidate-name-change.component.scss']
+})
+export class MiscCandidateNameChangeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/hr-dashboard/pages/misc-colleges/misc-colleges.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-misc-colleges',
+  templateUrl: './misc-colleges.component.html',
+  styleUrls: ['./misc-colleges.component.scss']
+})
+export class MiscCollegesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
