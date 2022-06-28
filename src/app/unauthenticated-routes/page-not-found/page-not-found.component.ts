fb1f53fdd189f86d54d228231b2da03b6923dda0
Avinash <Avinasi.selvakumar@lntedutech.com>
2022-06-28 15:41:26 +0530

    Error screen implementation done

--- src/app/unauthenticated-routes/page-not-found/page-not-found.component.ts
+++ src/app/unauthenticated-routes/page-not-found/page-not-found.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppConfigService } from 'src/app/config/app-config.service';
 import { CONSTANT } from 'src/app/constants/app-constants.service';
+import { ActivatedRoute } from '@angular/router';
+import { SharedServiceService } from 'src/app/services/shared-service.service';
 
 @Component({
   selector: 'app-page-not-found',
@@ -8,15 +10,24 @@ import { CONSTANT } from 'src/app/constants/app-constants.service';
   styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent implements OnInit {
+  redirectedFrom: any;
+  supportEmail = this.appConfig.supportEmailBasedOnCustomer();
+  isValidUser = this.appConfig.getLocalData('userEmail');
 
   constructor(
-    private appConfig: AppConfigService
+    private appConfig: AppConfigService,
+    private activatedRoute: ActivatedRoute,
+    private sharedService: SharedServiceService
   ) { }
 
   ngOnInit() {
   }
 
   routeToHome() {
+    this.appConfig.routeNavigation('/');
+  }
+
+  logout() {
     this.appConfig.routeNavigation('/');
   }
 }
