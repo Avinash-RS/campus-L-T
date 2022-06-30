9293937dd1e1f2be50eeed0eed868a897ec6bd50
Avinash <Avinasi.selvakumar@lntedutech.com>
2022-06-30 12:17:22 +0530

    Off campus data validation done

--- src/app/shared/off-campus/off-campus-profile/off-campus-profile.component.ts
+++ src/app/shared/off-campus/off-campus-profile/off-campus-profile.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RemoveWhitespace } from 'src/app/custom-form-validators/removewhitespace';
 import { AppConfigService } from 'src/app/config/app-config.service';
 import { GlobalValidatorService } from 'src/app/custom-form-validators/globalvalidators/global-validator.service';
-import { DateAdapter, MAT_DATE_LOCALE, MAT_DATE_FORMATS } from '@angular/material';
+import { DateAdapter, MAT_DATE_LOCALE, MAT_DATE_FORMATS, MatDialog } from '@angular/material';
 import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
 import moment from 'moment';
+import { CandidateMappersService } from 'src/app/services/candidate-mappers.service';
+import { LoaderService } from 'src/app/services/loader-service.service';
+import { Subscription } from 'rxjs';
 
 export const MY_FORMATS = {
   parse: {
@@ -37,7 +40,8 @@ export const MY_FORMATS = {
     { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
   ],
 })
-export class OffCampusProfileComponent implements OnInit {
+export class OffCampusProfileComponent implements OnInit, OnDestroy {
+  @ViewChild('matDialogTerms', { static: false }) matDialogRefTerms: TemplateRef<any>;
 
   offCampusRegistrationForm: FormGroup;
 
@@ -81,17 +85,28 @@ export class OffCampusProfileComponent implements OnInit {
   form_label = 'label';
   minDateDOB: Date;
   passportDateOfIssueMaxDate: Date;
+  getAllEducationFormDropdownListSubscription: Subscription;
+  ugQualificationList: any;
+  pgQualificationList: any;
+  ugDisciplineList: any;
+  pgDisciplineList: any;
+  ugInstitutesList: any;
+  pgInstitutesList: any;
 
 
 
   constructor(
     private fb: FormBuilder,
     private appConfig: AppConfigService,
-    private gv: GlobalValidatorService
+    private gv: GlobalValidatorService,
+    private candidateService: CandidateMappersService,
+    private loadingService: LoaderService,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit() {
     this.registrationFormInit();
+    this.educationDropdownValues();
   }
 
   dateValidation() {
@@ -120,6 +135,26 @@ export class OffCampusProfileComponent implements OnInit {
       }
       return split == 'Invalid date' ? null : split;
     }
+  }
+
+  educationDropdownValues() {
+    const api = {
+      level: '',
+      discipline: '',
+      specification: ''
+    };
+   this.getAllEducationFormDropdownListSubscription = this.candidateService.getAllEducationFormDropdownList(api).subscribe((data: any) => {
+      this.ugQualificationList = data && data.ug_specifications ? data.ug_specifications : [];
+      this.pgQualificationList = data && data.pg_specifications ? data.pg_specifications : [];
+      this.ugDisciplineList = data && data.ug_disciplines ? data.ug_disciplines : [];
+      this.pgDisciplineList = data && data.pg_disciplines ? data.pg_disciplines : [];
+      const list = data && data.ug_pg_colleges ? data.ug_pg_colleges : [];
+      this.ugInstitutesList = list;
+      const exceptOthers = list.filter((data: any) => data.college_name !== 'Others');
+      this.pgInstitutesList = exceptOthers;
+    }, (err) => {
+
+    });
   }
 
 
@@ -156,6 +191,85 @@ export class OffCampusProfileComponent implements OnInit {
       [this.form_file_type]: [null],
       [this.form_file_id]: [null],
     })
+  }
+
+  matDialogOpenTerms() {
+    let termsAndCondtionsPopRef = this.dialog.open(this.matDialogRefTerms, {
+    width: '890px',
+    height: 'auto',
+    autoFocus: false,
+    closeOnNavigation: true,
+    disableClose: false,
+    panelClass: 'wrapper-kyc-terms'
+  });
+}
+
+closeBox() {
+  this.dialog.closeAll();
+}
+
+
+  removeFile() {
+    this[this.form_resume].patchValue({
+      [this.form_label]: 'Resume',
+      [this.form_file_size]: null,
+      [this.form_file_path]: null,
+      [this.form_file_name]: null,
+      [this.form_file_type]: null,
+      [this.form_file_id]:   null,
+    });
+  }
+  async uploadImage(file) {
+    try {
+      this.loadingService.setLoading(true);
+      const data = await (await this.candidateService.uploadJoiningDocs(file)).json();
+      if (data && data.error_code) {
+        console.log('coming1')
+        this.loadingService.setLoading(false);
+       return this.appConfig.nzNotification('error', 'Not Uploaded', 'Please try again');
+      }
+      this.loadingService.setLoading(false);
+      if (data && data.file_id) {
+        console.log('coming2');
+        this[this.form_resume].patchValue({
+          [this.form_label]: 'Resume',
+          [this.form_file_size]: data.file_size,
+          [this.form_file_path]: data.file_path,
+          [this.form_file_name]: data.file_name,
+          [this.form_file_type]: data.type,
+          [this.form_file_id]:   data.file_id,
+        });
+        return this.appConfig.nzNotification('success', 'Uploaded', 'Document uploaded successfully');
+      }
+    }  
+    catch (e) {
+      console.log('coming4', e);
+      this.loadingService.setLoading(false);
+      this.appConfig.nzNotification('error', 'Not Uploaded', 'Please try again');
+    }
+  }
+  
+  onSelectFile(event) {
+    const fd = new FormData();
+    this.filepath.markAsTouched();
+    if (event.target.files && (event.target.files[0].type.includes('application/pdf'))) {
+      if (event.target.files[0].size < 2000000) {
+        if (this.appConfig.minImageSizeValidation(event.target.files[0].size)) {
+        let image = event.target.files[0];
+
+        fd.append('user_id', this.appConfig.getLocalData('userId') ? this.appConfig.getLocalData('userId') : '');
+        fd.append('description', 'off-campus Resume');
+        fd.append('label', 'off-campus Resume');
+        fd.append('level', 'off-campus Resume');
+        fd.append('product_image', image);
+        this.uploadImage(fd);
+      }
+     } else {
+      this.appConfig.nzNotification('error', 'Not Uploaded', 'Maximum file size is 2 MB');
+     }
+    } else {
+      return this.appConfig.nzNotification('error', 'Invalid Format', 'Please upload .PDF files only');
+    }
   }
 
   get name() {
@@ -201,10 +315,16 @@ export class OffCampusProfileComponent implements OnInit {
     return this.offCampusRegistrationForm.get(this.form_resume);
   }
   get filepath() {
-    return this.offCampusRegistrationForm.get(this.form_resume[this.form_file_path]);
+    return this.offCampusRegistrationForm.get(`${this.form_resume}.${this.form_file_path}`);
+  }
+  get filename() {
+    return this.offCampusRegistrationForm.get(`${this.form_resume}.${this.form_file_name}`);
   }
   get t_c() {
     return this.offCampusRegistrationForm.get(this.form_t_c);
   }
 
+  ngOnDestroy() {
+    this.getAllEducationFormDropdownListSubscription ? this.getAllEducationFormDropdownListSubscription.unsubscribe() : '';
+  }
 }
